36d470b22a809051e168ae8d47d5850caaa1ff5a
evgenius081 <56554114+evgenius081@users.noreply.github.com>
2019-11-14 21:19:24 +0300

    Add files via upload

---
+++ intro/script.js
@@ -0,0 +1,55 @@
+// alert("Hello again!");
+
+//Types of data
+// 1. String "Hello" "I love JS" '9'
+// 2. Number 99 5.2 -36
+// 3. Boolean true false
+// 4. null
+// 5. undefined
+// 6. Object {}
+// 7. Symbol
+// 8. LongInt
+
+// var a = 14; 
+
+// _asddad
+// $asdads  naming
+// asdasd2
+
+// alert(a);
+// document.write(a);
+// console.log(a);
+
+// arifmetical operations
+// console.log(a%6);
+
+//logical operations
+// var b = 14;
+// console.log(a >= b);
+//
+
+// var a = 14,
+// b = 45,
+// c = a/2 + b;
+// console.log(b==c);
+// console.log(a+=c);
+// c = c + 8; 
+// console.log(c);
+
+// convertation 
+
+// to string
+// String(2);
+// console.log(2 + '');
+
+//to number
+// Number(null);
+// Number('24');
+// console.log(+'99');
+
+//to boolean
+// Boolean(5);
+// console.log(!!'evg');
+
+var a = +prompt("code please", 'default value');
+console.log("ur code is " + (a + 2) + ' lol');

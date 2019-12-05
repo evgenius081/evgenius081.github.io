a875dd7bd143a3b6034f7856fc9a796af35011c4
evgenius081 <56554114+evgenius081@users.noreply.github.com>
2019-12-05 21:10:16 +0300

    Add files via upload

---
+++ callbackdz/script.js
@@ -0,0 +1,17 @@
+// function checkLogin(userLogin, rightLogin, ok, notOk){
+//     if(userlogin === rightLogin){
+//         notOk();
+//     }else{
+//         ok();
+//     }
+// }
+
+// function ok (){
+//     console.log("ok");
+// }
+
+// function notOk (){
+//     console.log("not ok");
+// }
+
+// checkLogin("admin", "admin", ok, notOk);
